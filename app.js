ac42c9511f8493415e7c67585b899be5d8968e1a
Neha Dubey <008nehadubey@gmail.com>
2021-01-29 23:49:56 +0530

    Stopwatch created by vanilla js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 window.onload = function(){
 
+    console.log("hyy");
     var seconds = 00;
     var tens = 00;
     var appendTens = document.getElementById("tens");
     var appendSeconds = document.getElementById("seconds");
-    var buttonStart = document.getElementById("button-start");
-    var buttonStop = document.getElementById("button-start");
-    var buttonReset = document.getElementById("button-reset");
+    var buttonStart = document.getElementById('button-start');
+    var buttonStop = document.getElementById('button-stop');
+    var buttonReset = document.getElementById('button-reset');
 
     var Interval;
 
     buttonStart.onclick = function() {
-
+        console.log("hyyyyyyy");
         clearInterval(Interval);
         Interval = setInterval(startTimer, 10);
     }
